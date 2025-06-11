b59f0c1df0bc6abe41ce7a45956b82e1c63728b6
Jesús Casañas <jcasanas@mobilecomputing.com.ar>
2025-06-11 17:08:04 -0300

    moved not found to pages

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,4 @@
 export { default as Filters } from "./filters";
 export { default as Footer } from "./footer";
 export { default as Header } from "./header";
-export { default as NotFound } from "./not-found";
 export { default as TopBar } from "./top-bar";
